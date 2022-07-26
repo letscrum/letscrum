75886d4429c091890933cb0639d068196835e83f
Wilson Wu <iwilsonwu@gmail.com>
2022-07-26 23:01:00 +0800

    > try config

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -65,9 +65,9 @@ var cfg *ini.File
 // Setup initialize the configuration instance
 func Setup() {
 	var err error
-	cfg, err = ini.Load("conf/app.ini")
+	cfg, err = ini.Load("config/app.ini")
 	if err != nil {
-		log.Fatalf("setting.Setup, fail to parse 'conf/app.ini': %v", err)
+		log.Fatalf("setting.Setup, fail to parse 'config/app.ini': %v", err)
 	}
 
 	mapTo("app", AppSetting)
