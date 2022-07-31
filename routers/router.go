df2b214114a03e816d1f1dcf143df3ffa10e6f72
Wilson Wu <iwilsonwu@gmail.com>
2022-07-31 22:33:39 +0800

    > update project member db

--- routers/router.go
+++ routers/router.go
@@ -1,18 +1,9 @@
 package routers
 
 import (
-	"github.com/letscrum/letscrum/middlewares/jwt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/letscrum/letscrum/middlewares/jwt"
 
-	"github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
-
-	"github.com/letscrum/letscrum/pkg/export"
-	"github.com/letscrum/letscrum/pkg/qrcode"
-	"github.com/letscrum/letscrum/pkg/upload"
-	"github.com/letscrum/letscrum/routers/apis"
 	"github.com/letscrum/letscrum/routers/apis/v1"
 )
 
@@ -20,48 +11,16 @@ import (
 func InitRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-
-	r.StaticFS("/export", http.Dir(export.GetExcelFullPath()))
-	r.StaticFS("/upload/images", http.Dir(upload.GetImageFullPath()))
-	r.StaticFS("/qrcode", http.Dir(qrcode.GetQrCodeFullPath()))
-
-	r.POST("/auth", apis.GetAuth)
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	r.POST("/upload", apis.UploadImage)
 
 	apisV1 := r.Group("/apis/v1")
+
 	apisV1.Use()
 	{
 		apisV1.GET("/signin", v1.SignIn)
 	}
+
 	apisV1.Use(jwtMiddleware.JWT())
 	{
-		//获取标签列表
-		apisV1.GET("/tags", v1.GetTags)
-		//新建标签
-		apisV1.POST("/tags", v1.AddTag)
-		//更新指定标签
-		apisV1.PUT("/tags/:id", v1.EditTag)
-		//删除指定标签
-		apisV1.DELETE("/tags/:id", v1.DeleteTag)
-		//导出标签
-		r.POST("/tags/export", v1.ExportTag)
-		//导入标签
-		r.POST("/tags/import", v1.ImportTag)
-
-		//获取文章列表
-		apisV1.GET("/articles", v1.GetArticles)
-		//获取指定文章
-		apisV1.GET("/articles/:id", v1.GetArticle)
-		//新建文章
-		apisV1.POST("/articles", v1.AddArticle)
-		//更新指定文章
-		apisV1.PUT("/articles/:id", v1.EditArticle)
-		//删除指定文章
-		apisV1.DELETE("/articles/:id", v1.DeleteArticle)
-		//生成文章海报
-		apisV1.POST("/articles/poster/generate", v1.GenerateArticlePoster)
 		apisV1.POST("/projects", v1.CreateProject)
 		apisV1.GET("/projects", v1.ListProject)
 		apisV1.PUT("/projects/:name", v1.UpdateProject)
--- schemas/002.project.sql
+++ schemas/002.project.sql
@@ -14,10 +14,14 @@ CREATE TABLE `project` (
 
 CREATE TABLE `project_member` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(20) DEFAULT NULL,
+  `project_id` bigint(20) unsigned DEFAULT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_project_id_and_user_id` (`project_id`, `user_id`),
+  CONSTRAINT `fk_project_member_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
+  CONSTRAINT `fk_project_member_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `sprint` (
