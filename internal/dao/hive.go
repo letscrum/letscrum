1529b85a145d21b0da79c213abff418391ffe784
Wilson Wu <iwilsonwu@gmail.com>
2022-08-27 00:29:48 +0800

    > refactor compile success

--- internal/dao/hive.go
+++
@@ -1,9 +0,0 @@
-package dao
-
-//go:generate mockgen -destination mock/mock_hive.go --build_flags=--mod=mod github.com/daocloud/skoala/app/hive/internal/dao HiveDao,BookDao,RegistryDao
-
-// HiveDao is the interface for hive.
-type HiveDao interface {
-	RegistryDao() RegistryDao
-	BookDao() BookDao
-}
--- internal/service/project.go
+++ internal/service/project.go
@@ -1,11 +1,50 @@
 package projectservice
 
 import (
+	"context"
+	v1 "github.com/letscrum/letscrum/api/letscrum/v1"
 	projectV1 "github.com/letscrum/letscrum/api/project/v1"
 	userV1 "github.com/letscrum/letscrum/api/user/v1"
+	"github.com/letscrum/letscrum/internal/dao"
 	models2 "github.com/letscrum/letscrum/internal/model/projectmembermodel"
 	"github.com/letscrum/letscrum/internal/model/projectmodel"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
+
+type ProjectService interface {
+	Get(context.Context, *projectV1.GetProjectRequest) (*projectV1.GetProjectResponse, error)
+}
+
+type ProjectServer struct {
+	v1.UnimplementedProjectServer
+	dao dao.LetscrumDao
+}
+
+func NewProjectService(letscrumService *LetscrumService) *ProjectService {
+	return &ProjectServer{dao: hiveService.dao}
+}
+
+func (s *bookService) Get(ctx context.Context, req *v1alpha1.GetBookReq) (*v1alpha1.GetBookReply, error) {
+	book, err := s.dao.BookDao().Get(ctx, req.Uid, metav1alpha1.GetOptions{})
+	if err != nil {
+		result := status.Convert(err)
+		if result.Code() == codes.NotFound {
+			return nil, status.Errorf(codes.NotFound, "get book err: %s not found", req.Uid)
+		}
+		return nil, status.Error(codes.Unknown, err.Error())
+	}
+
+	return &v1alpha1.GetBookReply{
+		Uid:         book.UID,
+		Name:        book.Name,
+		Author:      book.Author,
+		Status:      book.Status,
+		IsPublished: book.IsPublished,
+		PublishedAt: timestamppb.New(book.PublishedAt),
+	}, nil
+}
 
 func Create(name string, displayName string, createdUserId int64) (int64, error) {
 	projectId, err := projectmodel.CreateProject(name, displayName, createdUserId)
