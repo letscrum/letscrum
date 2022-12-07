aeed56e9b3cda2c3c0af475ca8a9e8475a016f81
Wilson Wu <iwilsonwu@gmail.com>
2022-12-07 11:08:47 +0800

    fix schema

--- schema/003.workitem.sql
+++ schema/003.workitem.sql
@@ -2,28 +2,33 @@ Use letscrum;
 
 CREATE TABLE `epic` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `project_id` bigint(20) unsigned NOT NULL,
   `sprint_id` bigint(20) unsigned DEFAULT 0,
   `title` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
+  CONSTRAINT `fk_epic_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
   CONSTRAINT `fk_epic_sprint_id` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `feature` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `project_id` bigint(20) unsigned NOT NULL,
   `sprint_id` bigint(20) unsigned DEFAULT 0,
   `epic_id` bigint(20) unsigned DEFAULT 0,
   `title` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
+  CONSTRAINT `fk_feature_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
   CONSTRAINT `fk_feature_sprint_id` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`id`),
   CONSTRAINT `fk_feature_epic_id` FOREIGN KEY (`epic_id`) REFERENCES `epic` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `workitem` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `project_id` bigint(20) unsigned NOT NULL,
   `sprint_id` bigint(20) unsigned DEFAULT 0,
   `feature_id` bigint(20) unsigned DEFAULT 0,
   `title` varchar(50) NOT NULL,
@@ -32,12 +37,14 @@ CREATE TABLE `workitem` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
+  CONSTRAINT `fk_workitem_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
   CONSTRAINT `fk_workitem_sprint_id` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`id`),
   CONSTRAINT `fk_workitem_feature_id` FOREIGN KEY (`feature_id`) REFERENCES `feature` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `task` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `project_id` bigint(20) unsigned NOT NULL,
   `sprint_id` bigint(20) unsigned DEFAULT 0,
   `workitem_id` bigint(20) unsigned DEFAULT 0,
   `title` varchar(50) NOT NULL,
@@ -45,6 +52,7 @@ CREATE TABLE `task` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
+  CONSTRAINT `fk_task_project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
   CONSTRAINT `fk_task_sprint_id` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`id`),
   CONSTRAINT `fk_task_workitem_id` FOREIGN KEY (`workitem_id`) REFERENCES `workitem` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
