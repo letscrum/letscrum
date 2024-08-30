948d4988d6f77570c41b84c9a3fad4c810adc07e
Wilson Wu <iwilsonwu@gmail.com>
2024-08-30 19:13:31 +0800

    add sprint status model

---
+++ internal/model/sprint_status.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type SprintStatus struct {
+	SprintId           uuid.UUID `gorm:"column:sprint_id;type:uuid;primaryKey" json:"sprint_id,omitempty"`
+	SprintDate         time.Time `gorm:"column:sprint_date;primaryKey" json:"sprint_date,omitempty"`
+	StartWorkItemCount int32     `gorm:"column:start_work_item_count" json:"start_work_item_count,omitempty"`
+	EndWorkItemCount   int32     `gorm:"column:end_work_item_count" json:"end_work_item_count,omitempty"`
+	StartTaskCount     int32     `gorm:"column:start_task_count" json:"start_task_count,omitempty"`
+	EndTaskCount       int32     `gorm:"column:end_task_count" json:"end_task_count,omitempty"`
+	StartWorkHours     float64   `gorm:"column:start_work_hours" json:"start_work_hours,omitempty"`
+	EndWorkHours       float64   `gorm:"column:end_work_hours" json:"end_work_hours,omitempty"`
+	ForSprint          Sprint    `gorm:"foreignKey:SprintId" json:"for_sprint,omitempty"`
+}
